5795da2e0b3a99348b23ec41d50ba1ffd058d277
Dot <01kokliko@gmail.com>
2025-01-12 21:35:36 +0100

    #2 Base.c

--- #2 Base.c
+++ #2 Base.c
@@ -12,10 +12,15 @@ int main(void)
 
     printf("The sum of %d and %d = %d\n", number_one, number_two, sum);
 
-    float floating_number = 14.37;
-    int integer_number = 42;
-    double large_floating_number = 12345.6789;
-    char letter = 'a';
+    float floating_number;
+    int integer_number;
+    double large_floating_number;
+    char letter;
+
+    floating_number = 14.37;
+    integer_number = 42;
+    large_floating_number = 12345.6789;
+    letter = 'a';
 
     printf("%2.2f\n", floating_number);
     printf("%2.2d\n", integer_number);
